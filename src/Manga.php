cd2f798d05780d1533448077ce74860ec80aa620
Elia <elia@softwareseni.com>
2022-07-18 20:47:05 +0700

    add some new chaining methods

--- src/Manga.php
+++ src/Manga.php
@@ -6,10 +6,160 @@ use Eliabrian\MangadexApi\MangadexApi;
 
 class Manga extends MangadexApi
 {
+    private const AGGREGATE_ENDPOINT = '/aggregate';
+    private const FEED_ENDPOINT = '/feed';
     private const MANGA_ENDPOINT = '/manga';
     private const RELATION_ENDPOINT = '/relation';
     private const RANDOM_ENDPOINT = '/random';
     private const TAG_ENDPOINT = '/tag';
+
+    /**
+     * @var array
+     */
+    public $query = [];
+
+    /**
+     * @var string
+     */
+    public $id = '';
+
+    /**
+     * @var string
+     */
+    public $url =  self::MANGA_ENDPOINT;
+
+    /**
+     * Method chain an include query to the request
+     * 
+     * @param array $include
+     * 
+     * @return array
+     */
+    public function includes (array $include)
+    {
+        $this->query['includes'] = $include;
+        return $this;
+    }
+
+    /**
+     * Method chain a limit query to the request
+     * 
+     * @param int $limit
+     * 
+     * @return array
+     */
+    public function limit (int $limit)
+    {
+        $this->query['limit'] = $limit;
+        return $this;
+    }
+
+    /**
+     * Method chain an offset query to the request
+     * 
+     * @param int $offset
+     * 
+     * @return array
+     */
+    public function offset (int $offset)
+    {
+        $this->query['offset'] = $offset;
+        return $this;
+    }
+
+    /**
+     * Method chain a title query to the request
+     * 
+     * @param string $title
+     * 
+     * @return array
+     */
+    public function title (string $title)
+    {
+        $this->query['title'] = $title;
+        return $this;
+    }
+
+    /**
+     * Method chain a translated languages query to the request
+     * 
+     * @param array $languages
+     * 
+     * @return array
+     */
+    public function language (array $languages)
+    {
+        $this->query['translatedLanguage'] = $languages;
+        return $this;
+    }
+
+    /**
+     * Method chain a Manga ID
+     * 
+     * @param string $id
+     * 
+     * @return string
+     */
+    public function id (string $id)
+    {
+        $this->url .= '/' . $id;
+        return $this;
+    }
+
+    /**
+     * Method chain a aggregate to Manga ID request
+     * 
+     * @return string
+     */
+    public function aggregate ()
+    {
+        $this->url .= self::AGGREGATE_ENDPOINT;
+        return $this;
+    }
+
+    /**
+     * Method chain a relation to Manga ID request
+     * 
+     * @return string
+     */
+    public function withRelation ()
+    {
+        $this->url .= self::RELATION_ENDPOINT;
+        return $this;
+    }
+
+    /**
+     * Method chain a random manga to the request
+     * 
+     * @return string
+     */
+    public function random ()
+    {
+        $this->url .= self::RANDOM_ENDPOINT;
+        return $this;
+    }
+
+    /**
+     * Method chain manga tags to the request
+     * 
+     * @return string
+     */
+    public function tag ()
+    {
+        $this->url .= self::TAG_ENDPOINT;
+        return $this;
+    }
+
+    /**
+     * Method chain manga feeds to the request
+     * 
+     * @return string
+     */
+    public function feed ()
+    {
+        $this->url .= self::FEED_ENDPOINT;
+        return $this;
+    }
 
     /**
      * Get Manga Lists
@@ -19,80 +169,12 @@ class Manga extends MangadexApi
      * @return object
      * 
      */
-    public function getMangas (array $queryParams) : object
-    {
-        $query = $this->buildQueryParams($queryParams);
-        
-        $response = $this->client->request('GET', self::MANGA_ENDPOINT, [
-            'query' => $query,
+    public function get () : object
+    {   
+        $response = $this->client->request('GET', $this->url, [
+            'query' => $this->query,
         ]);
 
         return $this->handleResponse($response);
-    }
-
-    /**
-     * Get Manga by MangaDex ID
-     * 
-     * @param string $id
-     * @param bool $withRelationship
-     * @param array $queryParams
-     * 
-     * @return object
-     */
-
-    public function getMangaById (string $id, bool $withRelationship = false, array $queryParams = []) : object
-    {
-        $pathParam = $withRelationship ? $id . self::RELATION_ENDPOINT : $id;
-
-        $query = !empty($queryParams) ? $this->buildQueryParams($queryParams) : [];
-
-        $response = $this->client->request('GET', self::MANGA_ENDPOINT . '/' . $pathParam, [
-            'query' => $query,
-        ]);
-
-        return $this->handleResponse($response);
-    }
-
-    /**
-     * Get a random Manga
-     * 
-     * @param array $queryParams
-     * 
-     * @return object
-     */
-
-    public function getRandomManga (array $queryParams = []) : object
-    {
-        $query = $this->buildQueryParams($queryParams);
-
-        $response = $this->client->request('GET', self::MANGA_ENDPOINT . self::RANDOM_ENDPOINT, [
-            'query' => $query,
-        ]);
-
-        return $this->handleResponse($response);
-    }
-
-    /**
-     * Get all manga tags
-     * 
-     * @return object
-     */
-    public function getMangaTags () : object
-    {
-        $response = $this->client->request('GET', self::MANGA_ENDPOINT . self::TAG_ENDPOINT);
-
-        return $this->handleResponse($response);
-    }
-
-    /**
-     * TODO: Get manga volumes and chapters
-     * 
-     * @param array $queryParams
-     * 
-     * @return object
-     */
-    public function getMangaAggregate(Type $var = null)
-    {
-        //
     }
 }
--- tests/MangaTest.php
+++ tests/MangaTest.php
@@ -12,12 +12,9 @@ final class MangaTest extends TestCase
     {
         $mangadex = new Manga();
 
-        $queryParams = [
-            'limit' => 1,
-            'offset' => 0,
-        ];
-
-        $mangas = $mangadex->getMangas($queryParams);
+        $mangas = $mangadex->limit(1)
+            ->offset(0)
+            ->get();
 
         $this->assertEquals('ok', $mangas->result);
     }
@@ -29,7 +26,10 @@ final class MangaTest extends TestCase
     {
         $mangadex = new Manga();
 
-        $manga = $mangadex->getMangaById('a96676e5-8ae2-425e-b549-7f15dd34a6d8', false, ['includes' => ['artist']]);
+        $manga = $mangadex->id('a96676e5-8ae2-425e-b549-7f15dd34a6d8')
+            ->withRelation()
+            ->includes(['artist'])
+            ->get();
 
         $this->assertEquals('ok', $manga->result);
     }
@@ -41,8 +41,23 @@ final class MangaTest extends TestCase
     {
         $mangadex = new Manga();
 
-        $manga = $mangadex->getRandomManga();
+        $manga = $mangadex->random()
+            ->includes(['artist'])
+            ->get();
 
+        $this->assertEquals('ok', $manga->result);
+    }
+
+    /**
+     * Assert get a aggregate by manga ID
+     */
+    public function testGetMangaAggregate () : void
+    {
+        $mangadex = new Manga();
+        $manga = $mangadex->id('a96676e5-8ae2-425e-b549-7f15dd34a6d8')
+            ->aggregate()
+            ->get();
+        
         $this->assertEquals('ok', $manga->result);
     }
 
@@ -53,8 +68,24 @@ final class MangaTest extends TestCase
     {
         $mangadex = new Manga();
 
-        $tags = $mangadex->getMangaTags();
+        $tags = $mangadex->tag()
+            ->get();
 
         $this->assertEquals('ok', $tags->result);
+    }
+
+    /**
+     * Assert get manga feeds
+     */
+    public function testGetMangaFeeds () : void
+    {
+        $mangadex = new Manga();
+
+        $feeds = $mangadex->id('a96676e5-8ae2-425e-b549-7f15dd34a6d8')
+            ->feed()
+            ->language(['en'])
+            ->get();
+
+        $this->assertEquals('ok', $feeds->result);
     }
 }
